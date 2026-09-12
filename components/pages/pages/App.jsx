8b22a7edc4892bc7ab27764332a41219c65dcf70
yaasiin123 <yaasiinkhaliilismaaciilciise@gmail.com>
2026-09-12 20:19:42 +0300

    Create App component with routing and layout

---
+++ components/pages/pages/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Dashboard from './pages/Dashboard'
+import Students from './pages/Students'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="flex min-h-screen bg-gray-100">
+        <Sidebar />
+        <main className="flex-1 p-8">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/students" element={<Students />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
